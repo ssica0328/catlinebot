301f7561cbef3375e7b8143aece8676e7ff491ae
ssica0328 <82892760+ssica0328@users.noreply.github.com>
2022-11-07 00:33:44 +0800

    Update apps.py

--- catlinebot/cat_linebot/apps.py
+++ catlinebot/cat_linebot/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CatLinebotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cat_linebot'
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
