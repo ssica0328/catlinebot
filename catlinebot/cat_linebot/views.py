1abc46cee669a72688a4bb735944d4058e3b0cc9
skyblue302 <bluesky595209@gmail.com>
2022-11-08 15:11:44 +0800

    update file

--- catlinebot/cat_linebot/views.py
+++ catlinebot/cat_linebot/views.py
@@ -1,3 +1,57 @@
+from django.conf import settings
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 
 # Create your views here.
+
+from linebot import WebhookParser
+from linebot.models import *
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+
+#其他
+import time
+
+line_api = LineBotApi(channel_access_token=settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(channel_secret=settings.LINE_CHANNEL_SECRET)
+
+#主程式
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            handle_message(event)
+    
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
+
+
+def handle_message(event):
+    #得到user資訊
+    display_name = line_api.get_profile(event.source.user_id).display_name
+    userId = line_api.get_profile(event.source.user_id).user_id
+    now = time.ctime()
+
+    #分辨型別為text
+    if event.message.type == "text":
+        text = event.message.text
+    else:
+        typeofmessage = event.message.type
+        print(f'({typeofmessage}){display_name}:\n{now}')
+        line_api.reply_message(event.reply_token,TextMessage(text="I can't reply this."))
+        return 200
+    print(f'(text){display_name}:{text}\n{now}')
+
+    line_api.reply_message(event.reply_token,TextMessage(text=text))
