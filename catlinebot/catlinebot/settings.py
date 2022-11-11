3905ad6e09e459a326c8d63fb8e0594ab4171b81
skyblue302 <bluesky595209@gmail.com>
2022-11-11 17:27:48 +0800

    Update settings.py

--- catlinebot/catlinebot/settings.py
+++ catlinebot/catlinebot/settings.py
@@ -14,8 +14,9 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
