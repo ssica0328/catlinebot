540400b3ee334e75e9900d3375003ed9a5765398
skyblue302 <bluesky595209@gmail.com>
2022-11-22 15:24:03 +0800

    update files

--- catlinebot/cat_linebot/admin.py
+++ catlinebot/cat_linebot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from cat_linebot.models import *
+
+class User_Info_Admin(admin.ModelAdmin):
+    list_display = ('uid', 'name', 'pic_url', 'mtext', 'mdt', 'points')
+admin.site.register(User_Info,User_Info_Admin)
