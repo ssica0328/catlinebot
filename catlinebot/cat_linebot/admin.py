107d4ed3f73c6d7e3677218d30003cb3f668d318
ssica0328 <82892760+ssica0328@users.noreply.github.com>
2022-11-29 15:00:35 +0800

    Update admin.py

--- catlinebot/cat_linebot/admin.py
+++ catlinebot/cat_linebot/admin.py
@@ -12,11 +12,11 @@ class random_exam_Admin(admin.ModelAdmin):
 admin.site.register(random_exam,random_exam_Admin)
 
 class 乖乖吃飯_Admin(admin.ModelAdmin):
-    list_display = ['num', 'name','price', 'grans', 'protein', 'fat', 'carbo', 'phos', 'kcal', 'score']
+    list_display = ['num', 'name','price', 'grams', 'protein', 'fat', 'carbo', 'phos', 'kcal', 'score']
 admin.site.register(乖乖吃飯,乖乖吃飯_Admin)
 
 class 心靈雞湯_Admin(admin.ModelAdmin):
-    list_display = ['num', 'name','price', 'grans', 'protein', 'fat', 'carbo', 'phos', 'kcal', 'score']
+    list_display = ['num', 'name','price', 'grams', 'protein', 'fat', 'carbo', 'phos', 'kcal', 'score']
 admin.site.register(心靈雞湯,心靈雞湯_Admin)
 
 class pidan_Admin(admin.ModelAdmin):
